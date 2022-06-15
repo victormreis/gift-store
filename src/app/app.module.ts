7f86fb1fe83171bb6649483c3b12abdec670826d
Victor Martins <vitor_jhoul@hotmail.com>
2022-06-15 10:14:19 -0300

    Criação do projeto Instalação das dependencias iniciais Configuração das fontes Criação dos componentes iniciais Criação do component menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule,
